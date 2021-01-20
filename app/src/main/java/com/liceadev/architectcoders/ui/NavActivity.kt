66e502ee2dee23578b683cc34fadfb3a67a59241
LiceaDev <licea.developer@gmail.com>
2021-01-19 19:50:11 -0600

    add nav host activity

---
+++ app/src/main/java/com/liceadev/architectcoders/ui/NavActivity.kt
@@ -0,0 +1,12 @@
+package com.liceadev.architectcoders.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.liceadev.architectcoders.R
+
+class NavActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav)
+    }
+}
