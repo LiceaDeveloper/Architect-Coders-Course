909a1570f370a731d072a8a5439b1d38af74a665
LiceaDev <licea.developer@gmail.com>
2021-03-02 13:52:10 -0600

    super merge

--- app/src/main/java/com/liceadev/architectcoders/data/server/UnsplashDataSource.kt
+++ app/src/main/java/com/liceadev/architectcoders/data/server/UnsplashDataSource.kt
@@ -1,15 +1,15 @@
 package com.liceadev.architectcoders.data.server
 
+import com.liceadev.architectcoders.data.mappers.toDomainPhoto
 import com.liceadev.data.source.RemoteDataSource
 import com.liceadev.domain.Photo as DomainPhoto
 
-class UnsplashDataSource : RemoteDataSource {
+class UnsplashDataSource(private val  photoClient:PhotoClient) : RemoteDataSource {
 
     override suspend fun getPhotos(apiKey: String, country: String): List<DomainPhoto> =
-        PhotoClient
+        photoClient
             .service
             .searchPhotos(apiKey, country, 30)
             .results
             .map { it.toDomainPhoto() }
 }
-
